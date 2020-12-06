465fcd4fd34acefce914d07f29214f6fe9cea878
Siargey Kachanovich <siargey.kachanovich@gmail.com>
2020-12-06 23:48:39 +0900

    Added Python stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,8 @@
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Escape key to quit
 
+;; Remove the annoying freeze on Ctrl-z
+(global-unset-key (kbd "C-z"))
 
 ;; Global line and column number mode
 (column-number-mode)
@@ -33,6 +35,7 @@
 (unless package-archive-contents
   (package-refresh-contents))	        ; Useful for the very first run
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))	; Install use-package
 (require 'use-package)
 (setq use-package-always-ensure t)     ; Download and install the packages before they are use-package'd 
@@ -73,7 +76,10 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-city-lights t)
-
+  (set-face-foreground 'font-lock-doc-face "SpringGreen3")
+  (set-face-foreground 'font-lock-comment-face "SeaGreen4")
+  (set-face-foreground 'font-lock-function-name-face "yellow3")
+  (set-face-background 'default "gray10")
   ;; Enable flashing mode-line on errors
   ; (doom-themes-visual-bell-config)
   
@@ -128,8 +134,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js-indent-level 2)
  '(package-selected-packages
-   '(unicode-fonts helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline diminish swiper ivy use-package)))
+   '(dumb-jump counsel-projectile projectile add-node-modules-path flycheck web-mode xref-js2 js2-refactor js2-mode unicode-fonts helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline diminish swiper ivy use-package))
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -141,6 +150,10 @@
 ;; (use-package unicode-fonts
 ;;   :config
 ;;   (unicode-fonts-setup))
+
+(use-package magit
+  :bind (("C-x g" . magit-status)))
+
 
 ;; Python: autodetect the indentation style
 (use-package python
@@ -164,8 +177,7 @@
       count)))
 
 (defun infer-indentation-style ()
-  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
-  ;; neither, we use the current indent-tabs-mode                               
+  "if our source file uses tabs, we use tabs, if spaces spaces, and if neither, we use the current 'indent-tabs-mode'" 
   (let ((space-count (how-many-region (point-min) (point-max) "^  "))
         (tab-count (how-many-region (point-min) (point-max) "^\t")))
     (if (> space-count tab-count) (setq indent-tabs-mode nil))
@@ -179,6 +191,100 @@
 	    (setq indent-tabs-mode nil)
 	    (infer-indentation-style)))
 
+
+;; JS-2
+(use-package js2-mode
+  :config
+  ;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode))
+(use-package js2-refactor
+  :config
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+ )
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+(use-package xref-js2
+  :config
+  (add-hook 'js2-mode-hook
+            (lambda () (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+ )
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+
+;; React (web-mode)
+(use-package web-mode
+  :custom
+  (web-mode-attr-indent-offset nil)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+  (web-mode-sql-indent-offset 2)
+  (indent-tabs-mode nil)
+  (tab-width 2)
+  :config
+  (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) ;; auto-enable for .js/.jsx files
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+  )
+
+;; (defun web-mode-init-hook ()
+;;   "Hooks for Web mode.  Adjust indent."
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2))
+;; (add-hook 'web-mode-hook 'web-mode-init-hook)
+
+;; Flycheck
+(use-package flycheck
+  :config
+  (setq-default flycheck-disabled-checkers
+		(append flycheck-disabled-checkers
+			'(javascript-jshint json-jsonlist)))
+  ;; Enable eslint checker for web-mode
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+  ;; Enable flycheck globally
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  )
+
+;; Enable eslint checker when web-mode is activated:
+(use-package add-node-modules-path
+  :config
+  (add-hook 'flycheck-mode-hook 'add-node-modules-path)
+ )
+
+;; projectile for project management
+(use-package projectile
+:ensure t
+:config
+(projectile-mode)
+(setq projectile-completion-system 'ivy)
+:bind (:map projectile-mode-map
+            ("C-c p" . projectile-command-map)))
+
+(use-package counsel-projectile
+:ensure t
+:config
+(counsel-projectile-mode))
+
+;; (use-package dumb-jump
+;; ;; :bind (("M-g o" . dumb-jump-go-other-window)
+;; ;; ("M-g j" . xref-find-definitions)
+;; ;; ("M-g x" . dumb-jump-go-prefer-external)
+;; ;; ("M-g z" . dumb-jump-go-prefer-external-other-window))
+;; :hook ((prog-mode . dumb-jump-mode))
+;; :config (setq dumb-jump-selector 'ivy)
+;; :init
+;; (dumb-jump-mode)
+;; )
+(use-package dumb-jump
+  :config
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+  (setq dumb-jump-selector 'ivy)
+  (setq dumb-jump-default-project "~/Corpy/Confide/confide-frontend"))
+
+;; Company
+(use-package company)
+;; (require 'company-web-html)
 
 ;; Backups
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
